b82b12516de5a4874e67c7ce1698d9ac0b14e83a
minhogang <minhogang@yahoo.com>
2018-12-06 17:28:19 +1000

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,6 +2,7 @@
 Express server running on port 3000. DO NOT RENAME OR MOVE THIS FILE */
 
 const proxy = require('http-proxy-middleware');
+//Proxy all requests to /api to port 3000 (Express server port)
 module.exports = (app) => {
     app.use(proxy('/api', {target: 'http://localhost:3000/'}
     ));
